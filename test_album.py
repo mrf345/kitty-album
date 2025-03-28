00abeab74cea94b1a72e34df5b03ee660b77d90e
Mohamed Feddad <mrf345@gmail.com>
2025-03-28 14:48:10 +0400

    test: add basic tests

---
+++ test_album.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import unittest, unittest.mock, pathlib
+
+from album import Album
+
+
+class TestAlbum(unittest.TestCase):
+    @unittest.mock.patch('album.ImageScaler')
+    @unittest.mock.patch('album.ImagesLoader')
+    @unittest.mock.patch('curses.initscr')
+    def setUp(self, mock_curses, mock_loader, mock_scaler):
+        self.path = pathlib.Path('./test_image.jpg')
+        self.album = Album(self.path)
+        self.album._window = unittest.mock.MagicMock()
+        self.album._files = [unittest.mock.MagicMock()] * 4
+
+        # Mock dependencies
+        self.mock_curses = mock_curses
+        self.mock_loader = mock_loader.return_value
+        self.mock_scaler = mock_scaler.return_value
+
+    def test_init(self):
+        """Test initialization of Album class"""
+        self.assertEqual(self.album._index, 0)
+        self.assertEqual(self.album._zoom_level, 0)
+        self.assertEqual(len(self.album.exit_keys), 3)
+        self.assertTrue(isinstance(self.album._scaler, unittest.mock.MagicMock))
+        self.mock_loader.start.assert_called_once()
+
+    def test_goto_next(self):
+        """Test moving to the next image"""
+        self.album.goto_next()
+        self.assertEqual(self.album.index, 1)
+        self.album.goto_next()
+        self.assertEqual(self.album.index, 2)
+
+    def test_goto_prev(self):
+        """Test moving to the previous image"""
+        self.album.index = 2
+        self.album.goto_prev()
+        self.assertEqual(self.album.index, 1)
+        self.album.goto_prev()
+        self.assertEqual(self.album.index, 0)
+
+    def test_goto_first(self):
+        """Test moving to the first image"""
+        self.album.index = 2
+        self.album.goto_first()
+        self.assertEqual(self.album.index, 0)
+
+    def test_goto_last(self):
+        """Test moving to the last image"""
+        self.album.index = 1
+        self.album.goto_last()
+        self.assertEqual(self.album.index, len(self.album._files) - 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
