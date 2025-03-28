22f67f766fa7a7e1d9bf0d2714e0720df6f84348
Mohamed Feddad <mrf345@gmail.com>
2025-03-28 14:54:33 +0400

    test: fix tests for emulated terminals

--- test_album.py
+++ test_album.py
@@ -18,6 +18,9 @@ class TestAlbum(unittest.TestCase):
         self.mock_curses = mock_curses
         self.mock_loader = mock_loader.return_value
         self.mock_scaler = mock_scaler.return_value
+        self.patch_terminal_size = unittest.mock.patch('album.os.get_terminal_size')
+        self.mock_terminal_size = self.patch_terminal_size.start()
+        self.addCleanup(self.patch_terminal_size.stop)
 
     def test_init(self):
         """Test initialization of Album class"""
@@ -26,6 +29,7 @@ class TestAlbum(unittest.TestCase):
         self.assertEqual(len(self.album.exit_keys), 3)
         self.assertTrue(isinstance(self.album._scaler, unittest.mock.MagicMock))
         self.mock_loader.start.assert_called_once()
+        self.mock_terminal_size.assert_not_called()
 
     def test_goto_next(self):
         """Test moving to the next image"""
@@ -33,6 +37,7 @@ class TestAlbum(unittest.TestCase):
         self.assertEqual(self.album.index, 1)
         self.album.goto_next()
         self.assertEqual(self.album.index, 2)
+        self.mock_terminal_size.assert_called()
 
     def test_goto_prev(self):
         """Test moving to the previous image"""
@@ -41,18 +46,21 @@ class TestAlbum(unittest.TestCase):
         self.assertEqual(self.album.index, 1)
         self.album.goto_prev()
         self.assertEqual(self.album.index, 0)
+        self.mock_terminal_size.assert_called()
 
     def test_goto_first(self):
         """Test moving to the first image"""
         self.album.index = 2
         self.album.goto_first()
         self.assertEqual(self.album.index, 0)
+        self.mock_terminal_size.assert_called()
 
     def test_goto_last(self):
         """Test moving to the last image"""
         self.album.index = 1
         self.album.goto_last()
         self.assertEqual(self.album.index, len(self.album._files) - 1)
+        self.mock_terminal_size.assert_called()
 
 
 if __name__ == '__main__':
